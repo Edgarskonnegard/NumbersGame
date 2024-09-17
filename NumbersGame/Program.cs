269f65935e69bbfff528fdb946423eef00d321b0
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2024-09-17 12:00:23 +0200

    Add project files.

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NumbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+            Random rng = new Random();
+            int num = rng.Next(1, 21);
+            int userGuess;
+            int count = 0;
+            while (count < 5)
+            {
+                userGuess = Convert.ToInt32(Console.ReadLine());
+                count++;
+
+            }
+
+        }
+    }
+}
